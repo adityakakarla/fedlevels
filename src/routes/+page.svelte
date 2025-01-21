283243c9c5fc4b099e32e9040f79f70ea6df3082
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-01-20 21:00:05 -0800

    hopefully mobile ui will be good

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	<h1 class="text-center p-2 mt-8 text-5xl font-light max-w-[600px]">
 		Building levels.fyi for government contractors.
 	  </h1>
-	<div class="mt-8 w-fit space-y-6 text-left">
+	<div class="mt-8 w-fit space-y-6 text-left hidden md:block">
 		{#each data.salaries as salary}
 			<div class='p-4 bg-stone-50 border border-stone-600 rounded-md hover:scale-105 transition duration-300 ease-in-out'>
 				<p class="text-lg">
@@ -21,4 +21,7 @@
 			</div>
 		{/each}
 	</div>
+	<button class='border border-black hover:text-white hover:bg-emerald-600 transition duration-300 ease-in-out mt-8 px-4 py-2'>
+		Get Started
+	</button>
 </div>
