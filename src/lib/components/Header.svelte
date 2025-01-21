c95dc33256d8bc92e0d51ae320f3298efe91a3e0
Aditya Kakarla <85147239+adityakakarla@users.noreply.github.com>
2025-01-20 20:52:31 -0800

    messed up mobile spacing

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,7 +7,7 @@
     ]
 </script>
 
-<div class='px-10 py-4 border-b-[1px] border-gray-700 space-x-6'>
+<div class='px-10 py-4 border-b-[1px] border-gray-700 space-x-4 md:space-x-6'>
 {#each links as link}
 <a href={link.path} class='hover:text-emerald-600 text-md md:text-xl font-light transition duration-50 ease-in-out'>{link.title}</a>
 {/each}
